efe87a144665680445f9a93d3387a3104de7954d
Gus Ryan <gus.ryan163@gmail.com>
2022-09-13 18:19:21 -0700

    Change from single page application to using router

---
+++ src/pages/portfolio.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/header"
+import Content from "../components/content"
+
+export default function Portfolio(): JSX.Element {
+	return (
+		<>
+			<Header />
+			<Content />
+		</>
+	)
+}
