ea906c279e790ee087f25892de041978dfdea150
Gus Ryan <gus.ryan163@gmail.com>
2022-09-23 12:05:28 -0700

    removing depreceated code

--- src/hooks/media.ts
+++ src/hooks/media.ts
@@ -12,27 +12,17 @@ export default function usePrefersReducedMotion(): boolean {
 		getMatches("(prefers-reduced-motion: reduce)")
 	)
 
-	function handleChange() {
+	function handleChange(): void {
 		setMatches(getMatches("(prefers-reduced-motion: reduce)"))
 	}
 
 	useEffect(() => {
 		const matchMedia = window.matchMedia("(prefers-reduced-motion: reduce)")
-
 		handleChange()
-
-		if (matchMedia.addListener) {
-			matchMedia.addListener(handleChange)
-		} else {
-			matchMedia.addEventListener("change", handleChange)
-		}
+		matchMedia.addEventListener("change", handleChange)
 
 		return () => {
-			if (matchMedia.removeListener) {
-				matchMedia.removeListener(handleChange)
-			} else {
-				matchMedia.removeEventListener("change", handleChange)
-			}
+			matchMedia.removeEventListener("change", handleChange)
 		}
 	}, [])
 
