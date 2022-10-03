c361d820ff57cf707769bf701119e3d42a3e6661
Gus Ryan <gus.ryan163@gmail.com>
2022-10-03 00:30:30 -0700

    open graph

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import "../styles/globals.scss"
 import { AppProps } from "next/app"
 import Head from "next/head"
 
+import { NextSeo } from "next-seo"
+
 import ReduxProvider from "../state/reduxProvider"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
@@ -17,6 +19,26 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/icon.png" />
 			</Head>
+			<NextSeo
+				title="Gus Ryan | Portfolio"
+				description="Come and check out my work!"
+				openGraph={{
+					url: "https://www.gusryan.me",
+					images: [
+						{
+							url: "/open-graph.jpg",
+							width: 800,
+							height: 600,
+							alt: "Hi I'm Gus Ryan. I enjoy creating things for the internet.",
+							type: "image/jpeg",
+						},
+					],
+				}}
+				twitter={{
+					cardType: "summary_large_image",
+				}}
+			/>
+
 			<ReduxProvider>
 				<Component {...pageProps} />
 			</ReduxProvider>
